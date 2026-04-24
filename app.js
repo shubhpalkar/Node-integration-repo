1d9a9d0e08945d458cfe518e794841a36f9ab570
Shubhangi Salunkhe <shubhangi.salunkhe1@dentsu.com>
2026-04-24 17:39:59 +0530

    agent fake url added

--- app.js
+++ app.js
@@ -1,21 +1,85 @@
 import express from "express";
+import crypto from "crypto";
+import bodyParser from "body-parser";
+import { config } from "./config.js";
+import { getPRFiles, postPRComment } from "./github.js";
+import { reviewCode } from "./agent.js";
 
 const app = express();
-app.use(express.json());
 
-// Test route
-app.get("/", (req, res) => {
-  res.send("Server is running 🚀");
+// Need raw body for signature verification
+app.use(
+  bodyParser.json({
+    verify: (req, res, buf) => {
+      req.rawBody = buf;
+    }
+  })
+);
+
+function verifySignature(req) {
+  const signature = req.headers["x-hub-signature-256"];
+  console.log("signature", signature)
+  if (!signature) return false;
+
+  const hmac = crypto.createHmac("sha256", config.webhookSecret);
+  const digest =
+    "sha256=" + hmac.update(req.rawBody).digest("hex");
+
+  return crypto.timingSafeEqual(
+    Buffer.from(signature),
+    Buffer.from(digest)
+  );
+}
+
+app.get("/", async(req, res) => {
+    return res.status(200).send("This is test data");
+})
+
+app.post("/webhook", async (req, res) => {
+  if (!verifySignature(req)) {
+    return res.status(401).send("Invalid signature");
+  }
+
+  const event = req.headers["x-github-event"];
+  const payload = req.body;
+
+  if (event === "pull_request") {
+    const action = payload.action;
+
+    if (action === "opened" || action === "synchronize") {
+      const { full_name } = payload.repository;
+      const [owner, repo] = full_name.split("/");
+      const prNumber = payload.pull_request.number;
+
+      console.log("full_name--", full_name)
+
+      try {
+        const files = await getPRFiles(owner, repo, prNumber);
+
+        const fullDiff = files
+          .filter(f => f.patch)
+          .map(f => `${f.filename}:\n${f.patch}`)
+          .join("\n\n");
+
+          console.log("fullDiff", fullDiff)
+
+        const review = await reviewCode(fullDiff);
+
+        console.log("review", review)
+
+        await postPRComment(owner, repo, prNumber, review);
+
+        return res.send({ status: "review posted" });
+      } catch (err) {
+        console.error(err);
+        return res.status(500).send("Error processing PR");
+      }
+    }
+  }
+
+  res.send({ status: "ignored" });
 });
 
-// Webhook route
-app.post("/webhook", (req, res) => {
-  console.log("Webhook received!");
-  //console.log(JSON.stringify(req.body, null, 2));
-
-  res.send("OK");
-});
-
-app.listen(8000, () => {
-  console.log("Server running on port 8000");
+app.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`);
 });
